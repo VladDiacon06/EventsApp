ecc750b39f9423456ae0b821cfcb1dacc9e13972
Anastezia15 <gavrilutsa.anastasia15@mail.ru>
2024-08-18 10:43:41 +0300

    EventsApplication v1.

---
+++ src/main/resources/db/changelog/db.changelog.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (firstname, lastname, email, password, username, birthday) VALUES ('John', 'Doe', 'john.doe@example.com', 'password123', 'johndoe', '1980-01-01');
+INSERT INTO users (firstname, lastname, email, password, username, birthday) VALUES ('Jane', 'Doe', 'jane.doe@example.com', 'password123', 'janedoe', '1990-02-01');
+INSERT INTO users (firstname, lastname, email, password, username, birthday) VALUES ('Bob', 'Smith', 'bob.smith@example.com', 'password123', 'bobsmith', '1985-03-01');
+INSERT INTO users (firstname, lastname, email, password, username, birthday) VALUES ('Alice', 'Johnson', 'alice.johnson@example.com', 'password123', 'alicejohnson', '1988-04-01');
+INSERT INTO users (firstname, lastname, email, password, username, birthday) VALUES ('Charlie', 'Brown', 'charlie.brown@example.com', 'password123', 'charliebrown', '1975-05-01');
